ccb27457725cb35f5c580b850af76414265fd033
Taylor Bowen <taylor.p.bowen@gmail.com>
2021-05-08 13:22:38 -0400

    added code to the javaScript, changed some ids in html.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function searchResults(resultObj){
+    var confirmed = resultObj.All.confirmed;
+    
+    var resultDiv = document.createElement('h3');
+    resultDiv.textContent = confirmed;
+  
+    var resultContent = document.getElementById("result-content");
+    resultContent.append(resultDiv);
+  }
+  
+  $("#search-form").on("submit", function(event) {
+      event.preventDefault()
+      var searchInput = $("#search-input").val();
+
+      console.log(searchInput);
+
+      searchCountry(searchInput)
+  });
+
+  function searchCountry(searchInput) {
+      $.ajax({
+          url:"https://covid-api.mmediagroup.fr/v1/cases?country=" + searchInput,
+          method: "GET"
+      }).then(function (apiResponse) {
+          console.log("todayForecastData", apiResponse);
+      })
+  }
