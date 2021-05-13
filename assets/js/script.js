a2810a901444823519b58fe7217359781079f697
Taylor Bowen <taylor.p.bowen@gmail.com>
2021-05-12 21:57:22 -0400

    added code for saving and using local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 //This function uses the country that was typed into the search box and uses that info to ask the API for covid cases in that country.
 $("#search-form").on("submit", function(event) {
       event.preventDefault()
+
       var searchInput = $("#search-input").val();
         
       searchInput = capitalizeFirstLetter(searchInput) //calling the function to set the proper format four country names in the API
 
       console.log(searchInput);
 
+      //Save country to local storage
+      saveData(searchInput);
+
       searchCountry(searchInput, 2)//telling this function to append this info into box 2(else statement)
 
   });
+
 //This function uses the API info and appends it to the HTML boxes. It also deciphers which box to append information into. 
   function searchResults(resultObj, outputID){
     var confirmed = resultObj.All.confirmed;
@@ -85,6 +90,25 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  //var save_button = document.getElementById('search-button')
+//save_button.onsubmit = saveData;
+
+function saveData(country){
+  localStorage.setItem("lastCountrySaved", country);
+}
+
+function retrieveLastCountry(){
+    //Logic here gets lastCountrySaved if it exists in local storage
+    var lastCountrySaved = localStorage.getItem("lastCountrySaved");
+    console.log(lastCountrySaved)
+    if (lastCountrySaved) {
+    searchCountry(lastCountrySaved, 2)
+    }
+    //call the api with the value
+    //searchCountry(nameOfCountry, which box #1 or #2)
+    
+}
+retrieveLastCountry()
 showLocalCases();
 
 
